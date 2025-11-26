e53e69a23717eb60d1654b9b90b6924c484e53ed
Saurabh-1785 <saurabhchauhan1785@gmail.com>
2025-11-26 18:55:40 +0000

    added model file

---
+++ models/postModel.js
@@ -0,0 +1,28 @@
+
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'A post must have a title.'],
+    trim: true 
+  },
+
+  markdownContent: {
+    type: String,
+    required: [true, 'A post must have content.'] // with custom error message
+  },
+
+  author: {
+    type: String,
+    default: 'Admin'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
